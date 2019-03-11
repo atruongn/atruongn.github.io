3300be2e2403a1b66c95ce939b82e1fe0d1ff15d
atruongn <29056894+atruongn@users.noreply.github.com>
2019-03-11 02:19:44 -0500

    Update flux.js

--- flux.js
+++ flux.js
@@ -4,7 +4,8 @@ myQuotes[0] = "In a constant state of <a target='_blank' href='https://open.spot
 myQuotes[1] = "Check out my work to get a little <a target='_blank'  href='https://open.spotify.com/album/1ePkYcH5ZQCb1b4tQeiEDj'>piece of me</a>.";
 myQuotes[2] = "Kinda far from the <a target='_blank' href='https://open.spotify.com/track/6QfS2wq5sSC1xAJCQsTSlj'>shallow</a> now.";
 myQuotes[3] = "Looking for <a target='_blank' href='https://open.spotify.com/track/7ARveOiD31w2Nq0n5FsSf8'>perfect places</a>.";
-
+myQuotes[4] = "Probably thinking about what I did <a target='_blank' href='https://open.spotify.com/track/3oHNJECGN3bBoGXejlw2b1'>last Friday night</a>.";
+  
 var myRandom = Math.floor(Math.random()*myQuotes.length);
 
 document.getElementById('myQuote').innerHTML= myQuotes[myRandom];
