162b6b923415d96d16fd3dcb512340b8c9acebaa
Anthony <atruongn15@gmail.com>
2019-03-16 16:51:44 -0500

    h1 font-size + quote update

--- flux.js
+++ flux.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
 var myQuotes = new Array();
 myQuotes[0] = "In a constant state of <a target='_blank' href='https://open.spotify.com/track/4zb3kFL9fXrrfTwIY4CcDy'>flux</a>.";
-myQuotes[1] = "Check out my work to get a <a target='_blank'  href='https://open.spotify.com/album/1ePkYcH5ZQCb1b4tQeiEDj'>piece of me</a>.";
+myQuotes[1] = "Check out some /work to get a <a target='_blank'  href='https://open.spotify.com/album/1ePkYcH5ZQCb1b4tQeiEDj'>piece of me</a>.";
 myQuotes[2] = "Kinda far from the <a target='_blank' href='https://open.spotify.com/track/6QfS2wq5sSC1xAJCQsTSlj'>shallow</a> now.";
 myQuotes[3] = "Looking for <a target='_blank' href='https://open.spotify.com/track/7ARveOiD31w2Nq0n5FsSf8'>perfect places</a>.";
 myQuotes[4] = "Probably thinking about what I did <a target='_blank' href='https://open.spotify.com/track/3oHNJECGN3bBoGXejlw2b1'>last Friday night</a>.";
