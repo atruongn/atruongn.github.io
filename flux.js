1894ad74be2033cedfb9ba699157c6ddcb9360d2
atruongn <29056894+atruongn@users.noreply.github.com>
2021-09-21 15:29:36 -0700

    Update flux.js

--- flux.js
+++ flux.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
 var myQuotes = new Array();
 myQuotes[0] = "In a constant state of <a target='_blank' href='https://open.spotify.com/track/4zb3kFL9fXrrfTwIY4CcDy'>flux</a>.";
-myQuotes[1] = "Check out /work to get a <a target='_blank'  href='https://open.spotify.com/track/2EvwLVrnYbCZEG6Kx5DCRy'>piece of me</a>.";
+myQuotes[1] = "Get a <a target='_blank'  href='https://open.spotify.com/track/2EvwLVrnYbCZEG6Kx5DCRy'>piece of me</a> in /work.";
 myQuotes[2] = "Kinda far from the <a target='_blank' href='https://open.spotify.com/track/6QfS2wq5sSC1xAJCQsTSlj'>shallow</a> now.";
 myQuotes[3] = "Looking for <a target='_blank' href='https://open.spotify.com/track/7ARveOiD31w2Nq0n5FsSf8'>perfect places</a>.";
 myQuotes[4] = "Probably <a target='_blank' href='https://open.spotify.com/track/7r5mUkmwLRXTtuoJEzJboP'>dancing on my own</a>.";
